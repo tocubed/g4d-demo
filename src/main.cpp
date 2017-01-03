db2b9a07615f2efa0849a9be132f3c4e562ea1a1
Priyank Patel <tocubed@gmail.com>
2017-01-02 23:09:51 -0800

    Add loading procedurally generated mesh from file

--- src/main.cpp
+++ src/main.cpp
@@ -135,43 +135,28 @@ VertexLayout Vertex::vertex_layout;
 
 void initModel()
 {
-	static const float hypercube[] = {
-		  -0.5,  -0.5,  -0.5,  -0.5,
-		  -0.5,  -0.5,  -0.5,   0.5,
-		  -0.5,  -0.5,   0.5,  -0.5,
-		  -0.5,  -0.5,   0.5,   0.5,
-		  -0.5,   0.5,  -0.5,  -0.5,
-		  -0.5,   0.5,  -0.5,   0.5,
-		  -0.5,   0.5,   0.5,  -0.5,
-		  -0.5,   0.5,   0.5,   0.5,
-		   0.5,  -0.5,  -0.5,  -0.5,
-		   0.5,  -0.5,  -0.5,   0.5,
-		   0.5,  -0.5,   0.5,  -0.5,
-		   0.5,  -0.5,   0.5,   0.5,
-		   0.5,   0.5,  -0.5,  -0.5,
-		   0.5,   0.5,  -0.5,   0.5,
-		   0.5,   0.5,   0.5,  -0.5,
-		   0.5,   0.5,   0.5,   0.5
-	};
+	std::ifstream model_file("../models/germa.m4d", std::ios::binary);
 
-	static const float texcoords[] = {
-		  0,  0,  0,  
-		  0,  0,  1, 
-		  0,  1,  0, 
-		  0,  1,  1, 
-		  1,  0,  0, 
-		  1,  0,  1, 
-		  1,  1,  0, 
-		  1,  1,  1, 
-		  1,  1,  1, 
-		  1,  1,  0, 
-		  1,  0,  1, 
-		  1,  0,  0, 
-		  0,  1,  1, 
-		  0,  1,  0, 
-		  0,  0,  1, 
-		  0,  0,  0
-	};
+	std::vector<Vertex> vertices;
+	std::uint32_t vertex_count;
+
+	model_file.read(reinterpret_cast<char*>(&vertex_count), 4);
+	for(std::uint32_t i = 0; i < vertex_count; i++)
+	{
+		Vertex vertex;
+		model_file.read(reinterpret_cast<char*>(vertex.position), 4 * 4);
+		model_file.read(reinterpret_cast<char*>(vertex.texcoord), 4 * 3);
+		vertices.push_back(vertex);
+	}
+	std::cout << vertex_count << " vertices\n";
+
+	std::vector<std::uint32_t> indices;
+	std::uint32_t index_count;
+
+	model_file.read(reinterpret_cast<char*>(&index_count), 4);
+	indices.resize(index_count);
+	model_file.read(reinterpret_cast<char*>(&indices[0]), 4 * index_count);
+	std::cout << index_count << " indices\n";
 
 	Vertex::vertex_layout
 		.setSize(sizeof(Vertex))
@@ -180,81 +165,12 @@ void initModel()
 		.add(VertexElement{offsetof(Vertex, texcoord), VertexAttribute::TexCoord,
 				VertexAttributeType::Float, 3, false, false});
 
-	std::vector<Vertex> vertices;
-	for(std::size_t i = 0; i < 16; i++)
-	{
-		vertices.push_back(Vertex{
-			{hypercube[4*i], hypercube[4*i+1], hypercube[4*i+2], hypercube[4*i+3]},
-			{texcoords[3*i], texcoords[3*i+1], texcoords[3*i+2]}});
-	}
-	
-	static const unsigned int indices[] = {
-		9 , 13 , 14 , 15 ,
-		12 , 9 , 13 , 14 ,
-		9 , 11 , 13 , 15 ,
-		11 , 9 , 14 , 15 ,
-		9 , 10 , 11 , 14 ,
-		12 , 9 , 14 , 8 ,
-		9 , 12 , 13 , 8 ,
-		9 , 10 , 14 , 8 ,
-		10 , 9 , 11 , 8 ,
-		6 , 2 , 4 , 0 ,
-		6 , 4 , 8 , 0 ,
-		2 , 6 , 8 , 0 ,
-		6 , 10 , 14 , 2 ,
-		12 , 6 , 14 , 4 ,
-		12 , 6 , 4 , 8 ,
-		6 , 12 , 14 , 8 ,
-		6 , 10 , 2 , 8 ,
-		10 , 6 , 14 , 8 ,
-		4 , 5 , 8 , 0 ,
-		1 , 5 , 4 , 0 ,
-		5 , 1 , 8 , 0 ,
-		9 , 5 , 13 , 1 ,
-		5 , 12 , 4 , 8 ,
-		12 , 5 , 13 , 8 ,
-		9 , 5 , 1 , 8 ,
-		5 , 9 , 13 , 8 ,
-		13 , 5 , 14 , 15 ,
-		7 , 5 , 13 , 15 ,
-		5 , 12 , 13 , 14 ,
-		5 , 7 , 14 , 15 ,
-		6 , 5 , 7 , 14 ,
-		5 , 12 , 14 , 4 ,
-		6 , 5 , 14 , 4 ,
-		5 , 6 , 7 , 4 ,
-		3 , 2 , 8 , 0 ,
-		1 , 3 , 8 , 0 ,
-		10 , 3 , 2 , 8 ,
-		3 , 10 , 11 , 8 ,
-		3 , 9 , 1 , 8 ,
-		9 , 3 , 11 , 8 ,
-		2 , 3 , 4 , 0 ,
-		3 , 1 , 4 , 0 ,
-		3 , 6 , 2 , 4 ,
-		6 , 3 , 7 , 4 ,
-		5 , 3 , 1 , 4 ,
-		3 , 5 , 7 , 4 ,
-		3 , 11 , 14 , 15 ,
-		7 , 3 , 14 , 15 ,
-		10 , 3 , 11 , 14 ,
-		3 , 6 , 7 , 14 ,
-		10 , 3 , 14 , 2 ,
-		3 , 6 , 14 , 2 ,
-		11 , 3 , 13 , 15 ,
-		3 , 7 , 13 , 15 ,
-		3 , 9 , 11 , 13 ,
-		5 , 3 , 7 , 13 ,
-		3 , 9 , 13 , 1 ,
-		5 , 3 , 13 , 1
-	};
-
 	display_mesh = std::make_unique<GL::GLDisplayMesh>();
 	display_mesh->begin();
-	display_mesh->setVertexCount(16);
-	display_mesh->setIndexCount(58 * 4);
+	display_mesh->setVertexCount(vertex_count);
+	display_mesh->setIndexCount(index_count);
 	display_mesh->addVertices(&vertices[0]);
-	display_mesh->addIndices(indices);
+	display_mesh->addIndices(&indices[0]);
 	display_mesh->end();
 }
 
@@ -297,7 +213,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	window = glfwCreateWindow(480, 320, "G4D Demo", nullptr, nullptr);
+	window = glfwCreateWindow(960, 720, "G4D Demo", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
